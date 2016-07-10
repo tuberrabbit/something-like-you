d25b4356d3438f9085d785b5143e55700301804d
tuber <btu@thoughtworks.com>
2016-07-10 18:05:39 +0800

    [tuber] add button group

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 "use strict";
-var DIALOG_CONTENT = 'gjslkdajfldsjalfjlsajglsjaflj';
+var DIALOG_CONTENT = 'DIALOG_CONTENT';
+var APPROVE_VALUE = 'DIALOG_CONTENT';
+var REJECT_VALUE = 'DIALOG_CONTENT';
+var APPROVE_CONTENT = 'DIALOG_CONTENT';
 
 var dialog = document.createElement('div');
 dialog.setAttribute('style',
     'background-color: wheat;' +
     'width: 300px;' +
-    'height: 100px;' +
+    'min-height: 100px;' +
     'position: absolute;' +
     'z-index: 1000;' +
     'top: 50%;' +
@@ -15,9 +18,43 @@ dialog.setAttribute('style',
     'border: 2px solid lightgray;' +
     'outline: double 4px blanchedalmond;');
 
-var content = document.createElement('div');
-content.innerHTML = DIALOG_CONTENT;
+var showApprove = function (flag) {
+    return flag && alert(APPROVE_CONTENT);
+};
 
-dialog.appendChild(content);
+
+var contentText = document.createElement('div');
+contentText.innerHTML = DIALOG_CONTENT;
+contentText.setAttribute('style',
+    'margin-bottom: 20px;');
+
+
+var approveBtn = document.createElement('button');
+approveBtn.textContent = APPROVE_VALUE;
+approveBtn.setAttribute('style',
+    'margin-left: 30px;');
+approveBtn.addEventListener('click', function (evt) {
+    showApprove(evt.target.textContent === APPROVE_VALUE);
+});
+
+
+var rejectBtn = document.createElement('button');
+rejectBtn.textContent = REJECT_VALUE;
+rejectBtn.addEventListener('click', function (evt) {
+    showApprove(evt.target.textContent === APPROVE_VALUE);
+});
+
+
+var buttonGroup = document.createElement('div');
+buttonGroup.setAttribute('style',
+    'position: absolute;' +
+    'bottom: 10px;' +
+    'right: 10px;');
+buttonGroup.appendChild(rejectBtn);
+buttonGroup.appendChild(approveBtn);
+
+
+dialog.appendChild(contentText);
+dialog.appendChild(buttonGroup);
 
 document.body.appendChild(dialog);
